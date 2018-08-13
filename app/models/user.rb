8e1bd5b464fba3e93891c48ba8d808eadee9dd19
paskuvan <majopaskuvan@gmail.com>
2018-08-12 23:07:13 -0300

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,3 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
   has_many :histories
-  validates :name, presence: true
-  validates :username, presence: true, uniqueness: true
-
-  def is_admin?
-    admin
-  end
 end
